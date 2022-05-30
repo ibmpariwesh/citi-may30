18ef32d08a9abb5d1fb9dbca333763ba49bb9e18
ibmpariwesh <pariweshg@gmail.com>
2022-05-30 11:40:12 +0530

    JAP support'

--- order-service/src/main/java/com/order/OrderController.java
+++ order-service/src/main/java/com/order/OrderController.java
@@ -1,13 +1,19 @@
 package com.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+	@Autowired
+	OrderService service;
 	@PostMapping("order") //end point
 	void createOrder(@RequestBody OrderVO order) {
+		service.saveOrder(order);
+		
 		System.out.println(order.getItem());
+		System.out.println(order.getQuantity());
 	}
 }
--- order-service/src/main/java/com/order/OrderVO.java
+++ order-service/src/main/java/com/order/OrderVO.java
@@ -1,9 +1,24 @@
 package com.order;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class OrderVO {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
 	String item;
 	float price;
+	int quantity;
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 	public String getItem() {
 		return item;
 	}
