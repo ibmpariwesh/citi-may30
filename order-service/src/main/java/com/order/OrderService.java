bf2acc687178f6831ac5c51893fa9c3dd5e2158a
ibmpariwesh <pariweshg@gmail.com>
2022-05-30 12:05:24 +0530

    get orders

--- order-service/src/main/java/com/order/OrderService.java
+++ order-service/src/main/java/com/order/OrderService.java
@@ -1,5 +1,7 @@
 package com.order;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,10 @@ public class OrderService {
 	
 	public void saveOrder(OrderVO order) {
 		orderRepository.save(order);
+	}
+
+	public List<OrderVO> getOrders() {
+		return orderRepository.findAll();
 	}
 	
 }
